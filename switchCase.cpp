d416169896ce487691167eac3e46030f3f64774d
arsalfadlan2-spec <arsalfadlan2@gmail.com>
2026-04-06 19:36:19 +0700

    Menambahkan variasi Global

--- switchCase.cpp
+++ switchCase.cpp
@@ -4,3 +4,15 @@ using namespace std;
 int main() {
     return 0;
 }
+#include <iostream>
+#include <string>
+using namespace std;
+
+// Deklarasi variabel global
+int kodeJurusan;
+string namaJurusan;
+string statusKuota;
+
+int main() {
+    return 0;
+}
