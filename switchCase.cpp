ebbb2e86a1a09af7cba93fbe6ccfed381ad0b2cb
arsalfadlan2-spec <arsalfadlan2@gmail.com>
2026-04-06 19:44:11 +0700

    Memanggil  main

--- switchCase.cpp
+++ switchCase.cpp
@@ -45,3 +45,16 @@ void pilihanJurusan() {
             statusKuota = "Error: Kode Tidak Valid";
     }
 }
+void output() {
+    cout << "\n=== HASIL SELEKSI ===" << endl;
+    cout << "Jurusan Pilihan : " << namaJurusan << endl;
+    cout << "Status Kuota    : " << statusKuota << endl;
+    cout << "----------------------------------" << endl;
+}
+int main() {
+    input();
+    pilihanJurusan();
+    output();
+
+    return 0;
+}
